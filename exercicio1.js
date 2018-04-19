9a70573b5bf8028a9dd1d9d5ca40b618953d12a4
Nícolas Maduro <nicolasamaduro@gmail.com>
2018-04-19 11:39:40 -0300

    pronto

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,14 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+botoes = document.getElementsByClassName('botao-expandir-retrair');
+for(let botao of botoes){
+	botao.addEventListener('click', function() {
+		if(botao.innerHTML==='+'){
+			botao.innerHTML='-';
+  			botao.parentNode.classList.add('expandido');
+		}else{			
+			botao.innerHTML='+';
+  			botao.parentNode.classList.remove('expandido');
+		}
+	});
+}
--- exercicio2.js
+++ exercicio2.js
@@ -9,3 +9,25 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+var indiceAtual=0;
+
+botoesAnterior = document.getElementById('anterior');
+botoesAnterior.addEventListener('click', function() {
+	if (indiceAtual===0){
+		indiceAtual = todasAsImagens.length-1;
+	}else{
+		indiceAtual--;
+	}
+    document.getElementById('slide').src=servidorDasImagens+todasAsImagens[indiceAtual];
+});
+
+
+botoesProximo = document.getElementById('proximo');
+botoesProximo.addEventListener('click', function() {
+	if (indiceAtual===todasAsImagens.length-1){
+		indiceAtual = 0;
+	}else{
+		indiceAtual++;
+	}
+    document.getElementById('slide').src=servidorDasImagens+todasAsImagens[indiceAtual];
+});
